522ac83921be20ddba25588727a51c49d0954a5c
Siddant <siddantgurung@hotmail.co.uk>
2019-04-25 20:27:16 +0100

    parallex works

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Slider from './common/Slider'
 import NewsSection from './common/NewsSection'
-
+import { Parallax } from "react-parallax";
+const image1 =
+    "https://images.unsplash.com/photo-1498092651296-641e88c3b057?auto=format&fit=crop&w=1778&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D";
 class Home extends React.Component {
     constructor() {
         super()
@@ -15,7 +17,7 @@ class Home extends React.Component {
                     <div className="hero-body">
                         <div className="container is-centered">
                             <h1 className="title is-1">
-                                Welocme to G-Link
+                                Welcome to G-Link
                             </h1>
                             <h2 className="subtitle is-3">
                                 A community for gamers
@@ -24,29 +26,34 @@ class Home extends React.Component {
                     </div>
                 </section>
                 <Slider title={"Hottest Game right now"} />
-                <section className="hero is-info is-large">
-                    <div className="hero-body">
-                        <div className="container">
-                            <h1 className="title">
-                                Large title
-                             </h1>
 
+                <Parallax bgImage="https://www.syfy.com/sites/syfy/files/wire/legacy/Uncharted4-Nathan-Drake.jpg" strength={350}>
+                    <section className="hero  is-large">
+                        <div className="hero-body">
+                            <div className="container">
+                                <h2 className="title is-4 is-white">
+                                    Explore
+                             </h2>
+
+                            </div>
                         </div>
-                    </div>
-                </section>
+                    </section>
+                </Parallax>
 
                 <Slider title={"Popular Streamers"} />
 
-                <section className="hero is-info is-large">
-                    <div className="hero-body">
-                        <div className="container">
-                            <h1 className="title">
-                                Large title
-      </h1>
+                <Parallax bgImage="https://www.gamersclassified.com/wp-content/uploads/2018/11/Is-eSports-In-Schools-On-The-Way.jpeg" strength={350}>
+                    <section className="hero is-large">
+                        <div className="hero-body">
+                            <div className="container">
+                                <h2 className="title is-4 is-white">
+                                    Conenct
+                             </h2>
 
+                            </div>
                         </div>
-                    </div>
-                </section>
+                    </section>
+                </Parallax>
 
 
 
@@ -61,16 +68,18 @@ class Home extends React.Component {
 
 
 
-                <section className="hero is-info is-large">
-                    <div className="hero-body">
-                        <div className="container">
-                            <h1 className="title">
-                                Large title
-                            </h1>
+                <Parallax bgImage="https://compass-ssl.xbox.com/assets/dc/48/dc486960-701e-421b-b145-70d04f3b85be.jpg?n=Game-Hub_Content-Placement-0_New-Releases-No-Copy_740x417_02.jpg" strength={350}>
+                    <section className="hero  is-large">
+                        <div className="hero-body">
+                            <div className="container">
+                                <h2 className="title is-4 is-white">
+                                    Play
+                                </h2>
 
+                            </div>
                         </div>
-                    </div>
-                </section>
+                    </section>
+                </Parallax>
             </div>
         )
     }
