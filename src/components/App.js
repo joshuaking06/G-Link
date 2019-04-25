85da3131b02cba203d7bd4de31692e4a79942bae
Siddant <siddantgurung@hotmail.co.uk>
2019-04-25 23:27:13 +0100

    slider works

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+
+import "react-alice-carousel/lib/alice-carousel.css";
+
+
 // import components
 import Login from './auth/Login'
 import Home from './Home'
--- src/components/common/Slider.js
+++ src/components/common/Slider.js
@@ -1,30 +1,84 @@
 import React from 'react'
+import AliceCarousel from 'react-alice-carousel';
 
 import ImageCard from './ImageCard'
-const Slider = ({ title }) => {
-    return (
-        <section className="section">
-            <div className="container is-set-to-zero container-full-screen">
-                <h2 className="title is-4">{title}</h2>
-                <div className="columns">
-                    <div className="column">
-                        <ImageCard />
-                    </div>
-                    <div className="column">
-                        <ImageCard />
-                    </div>
-                    <div className="column">
-                        <ImageCard />
-                    </div>
-                    <div className="column">
-                        <ImageCard />
-                    </div>
+// const Slider = ({ title }) => {
 
+class Slider extends React.Component {
+    constructor() {
+        super()
+
+        this.items = [1, 2, 3, 4, 5, <button>helo</button>]
+
+        this.state = {
+            currentIndex: 0,
+            itemsInSlide: 1,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                1024: {
+                    items: 4
+                }
+            },
+            galleryItems: [<div className="column">
+                <ImageCard />
+            </div>, <div className="column">
+                <ImageCard />
+            </div>, <div className="column">
+                <ImageCard />
+            </div>, <div className="column">
+                <ImageCard />
+            </div>, <div className="column">
+                <ImageCard />
+            </div>, <div className="column"><button>helo</button></div>]
+        }
+
+
+        this.responsive = {
+            0: { items: 1 },
+            1024: { items: 4 }
+        }
+
+        // this.handleOnSlideChange = this.handleOnSlideChange.bind(this);
+
+    }
+    render() {
+        const { galleryItems, responsive, currentIndex } = this.state
+
+        return (
+            <section className="section" >
+                <div className="container is-set-to-zero container-full-screen">
+                    <h2 className="title is-4">{this.props.title}</h2>
+                    {/* <div className="columns">
+                        <div className="column">
+                            <ImageCard />
+                        </div>
+                        <div className="column">
+                            <ImageCard />
+                        </div>
+                        <div className="column">
+                            <ImageCard />
+                        </div>
+                        <div className="column">
+                            <ImageCard />
+                        </div>
+
+                    </div> */}
+                    <div className="columns">
+                        <AliceCarousel
+                            items={galleryItems}
+                            slideToIndex={currentIndex}
+                            responsive={responsive}
+                            dotsDisabled={true}
+                            infinite={false}
+                        />
+                    </div>
                 </div>
-            </div>
 
-        </section>
-    )
+            </section >
+        )
+    }
 }
 
 
