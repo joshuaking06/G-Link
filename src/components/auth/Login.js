0669a29dfc24a70ca5eefae09a23f87ad52132bb
joshuaking06 <joshua.king0692@gmail.com>
2019-04-25 17:02:01 +0100

    finished up setState function and styling

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -3,21 +3,41 @@ import React from 'react'
 export default class Login extends React.Component {
 	constructor() {
 		super()
+
+		this.state = {
+			email: '',
+			password: ''
+		}
+
+		this.onSubmit = this.onSubmit.bind(this)
+		this.onChange = this.onChange.bind(this)
+	}
+
+	onChange({ target: { name, value } }) {
+		this.setState({ [name]: value })
+	}
+
+	onSubmit(e) {
+		e.preventDefault()
 	}
 
 	render() {
+		const { email, password } = this.state
 		return (
-			<div className="section columns">
-				<div className="login container column is-3">
-					<div className="form">
+			<div className="section login-section columns">
+				<div className="column is-4 is-offset-4">
+					<form onSubmit={this.onSubmit} className="login-form">
 						{/* label and input for email */}
 						<div className="field">
-							<label class="label is-light has-text-centered">Email</label>
+							<label className="label is-light has-text-centered">Email</label>
 							<p className="control has-icons-left">
 								<input
-									className="input is-primary"
+									onChange={this.onChange}
+									className="input is-medium"
 									type="email"
 									placeholder="Email"
+									name="email"
+									value={email}
 								/>
 								<span className="icon is-small is-left">
 									<i className="fas fa-envelope" />
@@ -26,12 +46,15 @@ export default class Login extends React.Component {
 						</div>
 						{/* label and input for password */}
 						<div className="field">
-							<label class="label is-light has-text-centered">Password</label>
+							<label className="label is-light has-text-centered">Password</label>
 							<p className="control has-icons-left">
 								<input
-									className="input is-primary"
+									onChange={this.onChange}
+									className="input is-medium"
 									type="password"
 									placeholder="Password"
+									name="password"
+									value={password}
 								/>
 								<span className="icon is-small is-left">
 									<i className="fas fa-lock" />
@@ -41,12 +64,12 @@ export default class Login extends React.Component {
 						{/* button to login */}
 						<div className="field">
 							<p className="control">
-								<button className="button is-link is-inverted is-fullwidth is-outlined">
+								<button className="button is-link is-fullwidth is-outlined">
 									Login
 								</button>
 							</p>
 						</div>
-					</div>
+					</form>
 				</div>
 			</div>
 		)
