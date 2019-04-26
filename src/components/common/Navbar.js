4d16ad0a715712c98ff1d38ca7420b7f18d1aa64
joshuaking06 <joshua.king0692@gmail.com>
2019-04-26 21:48:12 +0100

    basic search done  and filters started

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -5,49 +5,55 @@ class NavBar extends React.Component {
 	render() {
 		return (
 			<nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation">
-				<div className="navbar-brand">
-					<Link className="navbar-item" to="/">
-						G-Link
-					</Link>
+				<div className="container">
+					<div className="navbar-brand">
+						<Link className="navbar-item" to="/">
+							G-Link
+						</Link>
 
-					{/* <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+						{/* <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                         <span aria-hidden="true"></span>
                         <span aria-hidden="true"></span>
                         <span aria-hidden="true"></span>
                     </a> */}
-				</div>
-				<div id="navbarBasicExample" className="navbar-menu">
-					<div className="navbar-start">
-						<Link className="navbar-item" to="/">
-							Link Up
-						</Link>
-						<Link className="navbar-item" to="/">
-							Popular Games
-						</Link>
-						<Link className="navbar-item" to="/">
-							Forums
-						</Link>
 					</div>
-					{/* login and sign up buttons */}
-					<div className="navbar-end">
-						<div className="field has-addons navbar-item">
-							<div className="control ">
-								<input className="input" type="text" placeholder="Find a game..." />
-							</div>
-							<div className="control">
-								<Link to="/search" className="button is-link is-outlined">
-									<i className="fas fa-search" />
-								</Link>
-							</div>
+					<div id="navbarBasicExample" className="navbar-menu">
+						<div className="navbar-start">
+							<Link className="navbar-item" to="/">
+								Link Up
+							</Link>
+							<Link className="navbar-item" to="/">
+								Popular Games
+							</Link>
+							<Link className="navbar-item" to="/">
+								Forums
+							</Link>
 						</div>
-						<div className="navbar-item">
-							<div className="buttons">
-								<Link className="button is-link is-outlined" to="/register">
-									<strong>Sign up</strong>
-								</Link>
-								<Link className="button is-link is-outlined" to="/login">
-									<strong> Log in </strong>
-								</Link>
+						{/* login and sign up buttons */}
+						<div className="navbar-end">
+							<div className="field has-addons navbar-item">
+								<div className="control ">
+									<input
+										className="input"
+										type="text"
+										placeholder="Find a game..."
+									/>
+								</div>
+								<div className="control">
+									<Link to="/search" className="button is-link is-outlined">
+										<i className="fas fa-search" />
+									</Link>
+								</div>
+							</div>
+							<div className="navbar-item">
+								<div className="buttons">
+									<Link className="button is-link is-outlined" to="/register">
+										Sign up
+									</Link>
+									<Link className="button is-link is-outlined" to="/login">
+										Log in
+									</Link>
+								</div>
 							</div>
 						</div>
 					</div>
