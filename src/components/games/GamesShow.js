0facf7a013cf1f8013ed4f2d2e236b5ba210fb34
joshuaking06 <joshua.king0692@gmail.com>
2019-05-03 19:54:09 +0100

    axios request works

--- src/components/games/GamesShow.js
+++ src/components/games/GamesShow.js
@@ -1,15 +1,43 @@
 import React from 'react'
 import axios from 'axios'
 
+const queryString = (id) => {
+	return `query{
+		getGame(id:${id}){
+			_id
+			id
+			name
+			rating
+			rating_count
+			url
+			summary
+			cover{ url }
+			screenshots { url }
+			videos { name video_id }
+			genres { name }
+			game_modes { name }
+			similar_games { name }
+		}
+	}`
+}
+
 export default class GamesShow extends React.Component {
-	constructor() {
-		super()
+	constructor(props) {
+		super(props)
+	}
+
+	componentDidMount() {
+		axios
+			.post('/api/graphql', { query: queryString(this.props.match.params.id) })
+			.then((data) => this.setState({ game: data.data.data.getGame }))
+			.catch((err) => console.log(err))
 	}
 
 	render() {
 		return (
 			<section className="section game-section">
 				<div className=" game-show container">
+					{/* refactor this to be a reusable component along with the search version */}
 					<section className="game-hero hero is-small is-centered">
 						<div className="hero-body">
 							<div className="container">
