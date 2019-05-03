ad2a3c3da8be7bd0d47ff4d71e0ce470f9f3087e
Siddant <siddantgurung@hotmail.co.uk>
2019-05-03 16:59:28 +0100

    on scroll doesnot work, gonna try with a button

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -7,6 +7,7 @@ import axios from 'axios'
 class News extends React.Component {
 	constructor() {
 		super()
+		this.handleScroll = this.handleScroll.bind(this)
 	}
 
 	componentDidMount() {
@@ -28,18 +29,26 @@ class News extends React.Component {
 			.post('/api/graphql', { query: queryString })
 			.then((data) => this.setState(data.data.data))
 	}
+	handleScroll(e) {
+		console.log('here2')
+
+		// let element = e.target
+		// if (element.scrollHeight - element.scrollTop === element.clientHeight) {
+		// 	// do something at end of scroll
+		// 	console.log('here')
+		// }
+	}
 
 	render() {
-		{ console.log(this.state) }
 		if (!this.state) {
 			return <h1>Loading....</h1>
 		}
 		return (
-			<div>
+			<div >
 				<section className="section has-margin">
-					<div className="container container-full-screen">
+					<div className="container container-full-screen" >
 						<h2 className="title is-4">Gaming News</h2>
-						<NewsSection news={this.state.getNews} />
+						<NewsSection news={this.state.getNews} scrollEvent={this.handleScroll} />
 						{/* //.filter(elemn => (elemn.content || elemn.title)	.replace(/[a-zA-Z\s,.1-9&\/\\#,+()$~%.'":*?<>{}[(.*?)\]]/gmi, '') !== '...')} /> */}
 
 					</div>
