80be4a9ffecef11912d65b3ef6f4b043653ad061
Siddant <siddantgurung@hotmail.co.uk>
2019-05-04 12:48:11 +0100

    statrted on the chroom

---
+++ models/chatroom.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+
+const messageSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.ObjectId, ref: 'User' },
+    text: { type: String, required: true, maxlength: 250 }
+}, {
+        timestamps: true
+    })
+
+
+//Data structure model
+const chatroomSchema = new mongoose.Schema({
+    user: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
+    message: [messageSchema]
+})
+
+chatroomSchema.set('toJSON', {
+    transform(doc, json) {
+        delete json.__v
+        return json
+    }
+})
+
+module.exports = mongoose.model('Chatroom', chatroomSchema)
