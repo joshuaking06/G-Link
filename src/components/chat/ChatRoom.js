6cc86a6f814c293736c7d5ad29c51054cc940cf2
Siddant <siddantgurung@hotmail.co.uk>
2019-05-06 15:52:49 +0100

    starting ont sending message through forum

--- src/components/chat/ChatRoom.js
+++ src/components/chat/ChatRoom.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import Message from './Message'
-// const messages = [1]
-// const socket = require('socket.io-client')(`http://localhost:4000`);
 
 class ChatRoom extends React.Component {
     constructor() {
         super()
         this.state = {
-            message: 'dd',
+            message: '',
             messages: [{ "_id": "5cb727fc08b7103341940947", "text": "hello" }]
         }
         this.handleSumbit = this.handleSumbit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
 
         global.socket.on('connect', function () {
             console.log('connected')
@@ -18,25 +17,31 @@ class ChatRoom extends React.Component {
         })
 
         global.socket.on('RECEIVE_MESSAGE', (msg) => {
-            console.log(this.state.messages)
             const messages = [...this.state.messages, msg]
-
             this.setState({ ...this.state, messages })
         }
         )
-
     }
+
+
+    handleChange({ target: { name, value } }) {
+        this.setState({ ...this.state, [name]: value })
+    }
+
     handleSumbit(e) {
         e.preventDefault()
         global.socket.emit('chat message', 'sending')
+        this.setState({ ...this.state, message: '' })
+
     }
 
     render() {
+        { console.log(this.state) }
         return (
             <div className="column">
                 <div className="inbox">
                     {this.state.messages.map((elemn, index) =>
-                        // console.log(elemn)
+
                         < Message key={index}  {...elemn} />
                     )
                     }
@@ -46,12 +51,12 @@ class ChatRoom extends React.Component {
                 <form className="form" onSubmit={this.handleSumbit}>
                     <div className="field has-addons">
                         <p className="control">
-                            <input className="input is-large" type="text" placeholder="Write your message..." />
+                            <input className="input is-large" name="message" value={this.state.message} onChange={this.handleChange} type="text" placeholder="Write your message..." />
                         </p>
                         <p className="control">
                             <button className="button is-link is-outlined is-large">
                                 Send
-                    </button>
+                            </button>
                         </p>
                     </div>
 
