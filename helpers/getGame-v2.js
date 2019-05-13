a306ffd18f94b7bb30d68bd5c4a150c95749c397
Siddant <siddantgurung@hotmail.co.uk>
2019-05-13 22:18:56 +0100

    refactored the code

--- helpers/getGame-v2.js
+++ helpers/getGame-v2.js
@@ -4,15 +4,15 @@ require('dotenv').config()
 
 // fields we will use to build our game Object, with the related query fields for each api call
 const fields = {
-    cover: 'cover.game, cover.height, cover.image_id, cover.url, cover.width',
+    cover: 'cover.*',
     artworks: 'artworks.game, artworks.height, artworks.image_id, artworks.url, artworks.width',
     genres: 'genres.id, genres.name, genres.slug',
     platforms: 'platforms.id, platforms.abbreviation, platforms.alternative_name, platforms.name',
     game_modes: 'game_modes.name, game_modes.slug, game_modes.id',
     screenshots: 'screenshots.*',
     videos: 'videos.*',
-    similar_games: 'similar_games.cover.game, similar_games.cover.height, similar_games.cover.image_id, similar_games.cover.url, similar_games.cover.width, similar_games.id, similar_games.name',
-    dlcs: 'dlcs.cover.game, dlcs.cover.height, dlcs.cover.image_id, dlcs.cover.url, dlcs.cover.width, dlcs.id, dlcs.name',
+    similar_games: 'similar_games.cover.*, similar_games.id, similar_games.name',
+    dlcs: 'dlcs.cover.*, dlcs.id, dlcs.name',
     player_perspectives: 'player_perspectives.id, player_perspectives.name, player_perspectives.slug',
     game:
         'fields summary, id, rating, rating_count, name, url, videos'
@@ -22,8 +22,6 @@ const fields = {
 const getGameDataFromApi = async (gameId) => {
     try {
         const queryString = `${fields.game}, ${fields.cover}, ${fields.artworks}, ${fields.genres}, ${fields.platforms}, ${fields.game_modes}, ${fields.screenshots}, ${fields.videos}, ${fields.similar_games}, ${fields.dlcs}, ${fields.player_perspectives};exclude tags;where id = ${gameId};`
-        // const queryString = ` ${fields.platforms}, ${fields.game_modes}, ${fields.screenshots}, ${fields.videos}, ${fields.similar_games}, ${fields.dlcs}, ${fields.player_perspectives};exclude tags;where id = ${gameId};`
-        // console.log(queryString)
         return await axios({
             url: 'https://api-v3.igdb.com/games',
             method: 'POST',
@@ -42,15 +40,8 @@ const getGameDataFromApi = async (gameId) => {
 
 // take the array of data and spread them into a larger game Object, to be returned
 const assignGameToObj = async (gameId) => {
-    console.log(gameId)
-    console.log("here")
-
     let game = await getGameDataFromApi(gameId)
-    console.log(game)
     return Object.assign({}, ...game.data)
-    // console.log(Object.assign({}, ...game.data))
-
-
 }
 
 // function to search through the database based on user input(queryString)
@@ -74,10 +65,3 @@ module.exports = {
     fetchGame: assignGameToObj,
     searchGames: searchGames
 }
-
-
-
-
-
-
-
